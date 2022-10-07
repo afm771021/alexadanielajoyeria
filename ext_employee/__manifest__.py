bae4a9edb6e815c3aa60872b1610140edc0ed401
Antonio Fernández <afm@mss.mx>
2022-10-07 14:47:49 -0500

    Eliminar modulo donde se validaba el id de empleados

--- ext_employee/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Ext Employee Module',
-    'version': '1.0',
-    'description': 'Add business requeriments to employee module',
-    'author': 'Mastermind Software Services',
-    'license': 'LGPL-3',
-    'depends': ['hr'],
-    'application': False,
-}
